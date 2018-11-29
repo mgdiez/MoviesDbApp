95fe47b449b72e652dd09c451b4204537fe9de3c
Marc González <marcgdiez@gmail.com>
2018-11-29 16:43:01 +0100

    Inject observeOn and subscribeOn @ Application scope

--- app/src/main/java/com/marcgdiez/moviedbapp/di/AppModule.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/di/AppModule.kt
@@ -5,6 +5,10 @@ import com.marcgdiez.moviedbapp.MoviesDbApp
 import com.marcgdiez.moviedbapp.view.di.MoviesComponent
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Named
 
 @Module(
     subcomponents = [
@@ -16,4 +20,11 @@ class AppModule {
     @Provides
     fun context(application: MoviesDbApp): Context = application.applicationContext
 
+    @Provides
+    @Named("observeOn")
+    fun observeOnScheduler(): Scheduler = AndroidSchedulers.mainThread()
+
+    @Provides
+    @Named("subscribeOn")
+    fun subscribeOnScheduler(): Scheduler = Schedulers.io()
 }
