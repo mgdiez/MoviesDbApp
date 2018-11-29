b49aaea7013f3fe0dab1a886a8f21fe9916a0b6d
Marc González <marcgdiez@gmail.com>
2018-11-29 19:03:12 +0100

    NetworkConfig class for const vals and NetworkModule to inject Retrofit dependencies

---
+++ app/src/main/java/com/marcgdiez/moviedbapp/data/NetworkConfig.kt
@@ -0,0 +1,9 @@
+package com.marcgdiez.moviedbapp.data
+
+object NetworkConfig {
+    const val API_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "fab7d97ae19573a00a05dcea995abddf"
+    const val API_LAN = "en-US"
+    const val API_BASE_IMAGE = "https://image.tmdb.org/t/p"
+    const val API_IMAGE_SIZE = "w500/"
+}
--- app/src/main/java/com/marcgdiez/moviedbapp/di/NetworkModule.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/di/NetworkModule.kt
@@ -1,12 +1,53 @@
 package com.marcgdiez.moviedbapp.di
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.marcgdiez.moviedbapp.data.MoviesApi
+import com.marcgdiez.moviedbapp.data.MoviesMapper
 import com.marcgdiez.moviedbapp.data.MoviesRepositoryImpl
+import com.marcgdiez.moviedbapp.data.NetworkConfig
 import com.marcgdiez.moviedbapp.domain.MoviesRepository
+import com.marcgdiez.moviedbapp.extensions.create
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 class NetworkModule {
+
     @Provides
-    fun googlePlacesRepository(): MoviesRepository = MoviesRepositoryImpl()
+    fun provideMoviesApi(retrofit: Retrofit): MoviesApi = retrofit.create()
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .apply {
+                addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                addConverterFactory(GsonConverterFactory.create())
+                baseUrl(NetworkConfig.API_URL)
+                client(okHttpClient)
+            }
+            .build()
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    @Provides
+    fun providesMapper(): MoviesMapper = MoviesMapper()
+
+    @Provides
+    fun googlePlacesRepository(moviesApi: MoviesApi, moviesMapper: MoviesMapper): MoviesRepository =
+        MoviesRepositoryImpl(moviesApi, moviesMapper)
 }
