6f252d98456cb733a7a006d4b4f125b3ad851717
Marc González <marcgdiez@gmail.com>
2018-11-30 08:35:20 +0100

    Fix ImageURL parsing

--- app/src/main/java/com/marcgdiez/moviedbapp/data/NetworkConfig.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/data/NetworkConfig.kt
@@ -4,6 +4,6 @@ object NetworkConfig {
     const val API_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "fab7d97ae19573a00a05dcea995abddf"
     const val API_LAN = "en-US"
-    const val API_BASE_IMAGE = "https://image.tmdb.org/t/p"
-    const val API_IMAGE_SIZE = "w500/"
+    const val API_BASE_IMAGE = "https://image.tmdb.org/t/p/"
+    const val API_IMAGE_SIZE = "w500"
 }
