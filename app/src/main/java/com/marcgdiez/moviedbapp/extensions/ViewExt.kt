2393d83143f681ec675226f481569c306a702bdf
Marc González <marcgdiez@gmail.com>
2018-11-30 08:30:19 +0100

    View extension to inflate layouts

---
+++ app/src/main/java/com/marcgdiez/moviedbapp/extensions/ViewExt.kt
@@ -0,0 +1,9 @@
+package com.marcgdiez.moviedbapp.extensions
+
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
+    LayoutInflater.from(context).inflate(layoutRes, this, false)
