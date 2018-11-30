af1db7a64bc7d7b20392fce4221e915a3de8df79
Marc González <marcgdiez@gmail.com>
2018-11-30 10:55:49 +0100

    View extension for hide/show views

--- app/src/main/java/com/marcgdiez/moviedbapp/extensions/ViewExt.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/extensions/ViewExt.kt
@@ -7,3 +7,11 @@ import android.view.ViewGroup
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
     LayoutInflater.from(context).inflate(layoutRes, this, false)
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
