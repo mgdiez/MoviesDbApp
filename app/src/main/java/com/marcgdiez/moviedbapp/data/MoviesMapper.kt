75f43db4dc5e52062600f0f0b7545d50ca2daf03
Marc González <marcgdiez@gmail.com>
2018-12-01 12:35:03 +0100

    Remove unused fields from Movie business object

--- app/src/main/java/com/marcgdiez/moviedbapp/data/MoviesMapper.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/data/MoviesMapper.kt
@@ -6,23 +6,23 @@ import com.marcgdiez.moviedbapp.domain.bo.Movie
 
 class MoviesMapper {
     fun map(getMoviesResponseDto: GetMoviesResponseDto): GetMoviesResponse =
-        GetMoviesResponse(
-            moviesDtoToBo(getMoviesResponseDto),
-            getMoviesResponseDto.total_pages
-        )
+            GetMoviesResponse(
+                    moviesDtoToBo(getMoviesResponseDto),
+                    getMoviesResponseDto.total_pages
+            )
 
     private fun moviesDtoToBo(getMoviesResponse: GetMoviesResponseDto): List<Movie> =
-        getMoviesResponse.results.map {
-            Movie(
-                generateBackdropImageUrl(it.backdrop_path), it.first_air_date,
-                it.genre_ids, it.id, it.name, it.origin_country, it.original_language, it.original_name, it.overview,
-                it.popularity, generatePosterImageUrl(it.poster_path), it.vote_average, it.vote_count
-            )
-        }
+            getMoviesResponse.results.map {
+                Movie(
+                        generateBackdropImageUrl(it.backdrop_path), it.first_air_date,
+                        it.id, it.name, it.overview, it.popularity, generatePosterImageUrl(it.poster_path),
+                        it.vote_average, it.vote_count
+                )
+            }
 
     private fun generatePosterImageUrl(url: String) =
-        NetworkConfig.API_BASE_IMAGE + NetworkConfig.API_POSTER_IMAGE_SIZE + url
+            NetworkConfig.API_BASE_IMAGE + NetworkConfig.API_POSTER_IMAGE_SIZE + url
 
     private fun generateBackdropImageUrl(url: String) =
-        NetworkConfig.API_BASE_IMAGE + NetworkConfig.API_BACKDROP_IMAGE_SIZE + url
+            NetworkConfig.API_BASE_IMAGE + NetworkConfig.API_BACKDROP_IMAGE_SIZE + url
 }
--- app/src/main/java/com/marcgdiez/moviedbapp/domain/bo/Movie.kt
+++ app/src/main/java/com/marcgdiez/moviedbapp/domain/bo/Movie.kt
@@ -7,12 +7,8 @@ import kotlinx.android.parcel.Parcelize
 data class Movie(
     val backdropPath: String,
     val firstAirDate: String,
-    val genreIds: List<Int>,
     val id: Int,
     val name: String,
-    val country: List<String>,
-    val language: String,
-    val originalName: String,
     val overview: String,
     val popularity: Double,
     val imageUrl: String,
